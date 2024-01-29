208a89e121875df60427095d489022420e587e05
Guapura89 <betocrack1228@gmail.com>
2024-01-28 21:06:51 -0700

    feat: add alert component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
+"use client";
+import { useAlertContext } from "@/context/AppContext";
+import LoginForm from "@/components/LoginForm";
+import { Alert } from "@/components/Alert";
 
 // 763dd0f7fc1df732f088620c03c54a7c
 
 export default function Home() {
+  const context = useAlertContext();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        
-      </div>
+    <main className="min-h-screen  relative p-0 sm:p-2 flex items-center bg-gradient-to-b lg:bg-gradient-to-r from-purple-500 to-pink-700">
+      <LoginForm />
+      <Alert props={context.alert} />
     </main>
   );
 }
