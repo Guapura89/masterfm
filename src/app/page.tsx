83026a0f4bcd2b9b3e426198abec5aa9f3e29b38
Guapura89 <betocrack1228@gmail.com>
2024-01-30 15:34:19 -0700

    feat: move alert component to login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 "use client";
-import { useAlertContext } from "@/context/AppContext";
-import LoginForm from "@/components/LoginForm";
-import { Alert } from "@/components/Alert";
+import Gallery from "@/components/Gallery";
+import { useRouter } from "next/navigation";
 
-// 763dd0f7fc1df732f088620c03c54a7c
-
-export default function Home() {
-  const context = useAlertContext();
+export default function Home(): JSX.Element {
+  const route = useRouter();
   return (
-    <main className="min-h-screen  relative p-0 sm:p-2 flex items-center bg-gradient-to-b lg:bg-gradient-to-r from-purple-500 to-pink-700">
-      <LoginForm />
-      <Alert props={context.alert} />
+    <main className="">
+      <div className="grid grid-rows-2 lg:grid-rows-1 lg:grid-cols-2 min-h-screen max-h-screen">
+        <div className="bg-dark lg:order-2 text-white flex flex-col items-start justify-center p-4 md:p-10">
+          <h1 className="my-4 md:text-4xl lg:text-6xl">
+            <strong>Discover the Soundtrack of Your Life</strong>
+          </h1>
+          <p className="my-4 md:text-xl lg:text-2xl">
+            Unleash your inner music with our beatifully curated collection of
+            albums
+          </p>
+
+          <button onClick={() => route.push('/login')} className="px-4 py-2 my-4 font-bold lg:w-1/3 lg:py-3 hover:bg-gradient-to-tr hover:text-white transition duration-150 ease-linear from-purple-500 to-pink-700 bg-white text-dark rounded-lg">
+            Explore Now
+          </button>
+        </div>
+        <div className="bg-[url('/background/wallpaper.jpg')] bg-cover bg-center"></div>
+      </div>
+
+      <Gallery />
     </main>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,14 +1,14 @@
 import { Fade } from "react-awesome-reveal";
 import { AlertState } from "@/hooks/useAlert";
 
-const Alert = ({ props }: { props: AlertState }) => {
+const Alert = ({ props }: { props: AlertState }): JSX.Element => {
   return (
     <Fade
       direction="down"
       duration={300}
       className={`absolute ${
         !props.hidden ? "" : "hidden"
-      } bg-white w-auto h-10 rounded-md p-2 border-b-4 border-green-400 right-10 top-10`}
+      } bg-white w-auto h-10 rounded-md p-2 border-b-4 border-red-400 right-10 top-10`}
     >
       <h1>{props.message}</h1>
     </Fade>
