cfd804699c3f10895095e0b81d113887088ac478
Guapura89 <betocrack1228@gmail.com>
2024-01-30 15:40:39 -0700

    build: add login route

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { useAlertContext } from "@/context/AppContext";
+import LoginForm from "@/components/LoginForm";
+import { Alert } from "@/components/Alert";
+
+// 763dd0f7fc1df732f088620c03c54a7c
+
+export default function Login() {
+  const context = useAlertContext();
+  return (
+    <main className="min-h-screen  relative p-0 sm:p-2 flex items-center bg-gradient-to-b lg:bg-gradient-to-r from-purple-500 to-pink-700">
+      <LoginForm />
+      <Alert props={context.alert} />
+    </main>
+  );
+}
